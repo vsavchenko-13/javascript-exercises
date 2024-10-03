cbc8c390fb22dad9b68dcdb81767a1b77005584e
vsavchenko-13 <v.savchenko1304@gmail.com>
2024-10-02 20:31:55 -0600

    Created functions and conditions for the test to succeed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+    let sum = 0;
 
+
+    if (Number.isInteger(min) && Number.isInteger(max) && min >= 0 && max >= 0) {
+        if (min < max) {
+            for (let i = min; i <= max; i++) {
+                sum = sum + i;
+            }
+        } else if (min > max) {
+            for (let i = max; i <= min; i++) {
+                sum = sum + i;
+            }
+        }
+    }
+    else {
+        return "ERROR";
+    }
+    return sum;
 };
 
 // Do not edit below this line
