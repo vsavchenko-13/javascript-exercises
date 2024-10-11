da0ad93a62aa57e22068d839f0477b6f31262db9
Vlad <vlad.savchenko@tourmalineoil.com>
2024-10-11 13:51:09 -0600

    Fibonaci numbers have been setup, have to still run npm test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let a = 0;
+    let b = 1;
+    let sum = 0;
+    for (let i = 1; i < num; i ++){
+        sum = 0;
+        sum = a + b;
+        a = b;
+        b = sum;
+    }
+    console.log(sum);
+    return sum;
 };
 
+fibonacci(4);
+
 // Do not edit below this line
-module.exports = fibonacci;
+//module.exports = fibonacci;
