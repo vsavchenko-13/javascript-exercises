d12051e3c4ecad7a277be9efcf686429fdaf4463
vsavchenko-13 <v.savchenko1304@gmail.com>
2024-10-10 18:15:14 -0600

    Added functionality for all of the functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a,b) {
+	let add = 0;
+  add = a + b;
+  return add;
+}
+
+const subtract = function(a,b) {
+	let subtract = 0;
+  subtract = a - b;
+  return subtract;
 };
 
-const subtract = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (let i = 0; i < arr.length; i++){
+    sum = sum + arr[i];
+  }
+  return sum;
 };
 
-const sum = function() {
-	
+const multiply = function(arr) {
+  let multiply = arr[0];
+  for (let i = 1; i < arr.length; i++){
+    multiply = multiply * arr[i];
+  }
+  return multiply;
 };
 
-const multiply = function() {
-
+const power = function(a,b) {
+	let answer = 1;
+  for(let i = 0; i < b; i++){
+    answer = answer * a;
+  }
+  return answer;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	let answer = 1;
+  for (let i = 1; i <= num; i++){
+    answer = answer * i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
