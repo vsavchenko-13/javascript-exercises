568cd5ffa760df8c0c4a6d8690503819606a8459
vsavchenko-13 <v.savchenko1304@gmail.com>
2024-10-10 20:54:27 -0600

    Most of the logic was built, have to flash it out still

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let palindrome = false;
+    let array = string.replace(",", "").split(" ");
+    //check reversed string vs last array object
+    for (let i = 0; i < array.length; i ++){
+        if (array[i].split("").reverse().join("") != array[string.length - i]){
+            return false;
+        }  else {
+            palindrome = true;
+        }  
+    }
+    return palindrome;
 };
+
+let word = "racecar";
+
+palindromes(word);
 
 // Do not edit below this line
 module.exports = palindromes;
