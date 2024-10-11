79aad9c09142abfbc2f9f6fc0e7ee3f9ba19a0bc
Vlad <vlad.savchenko@tourmalineoil.com>
2024-10-11 13:30:24 -0600

    Added functionality for palindromes, still have to test with NPM at home

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,20 +1,24 @@
 const palindromes = function (string) {
-    let palindrome = false;
-    let array = string.replace(",", "").split(" ");
-    //check reversed string vs last array object
-    for (let i = 0; i < array.length; i ++){
-        if (array[i].split("").reverse().join("") != array[string.length - i]){
+    let array = string.split(" ");
+    if (array.length == 1) {
+        let word = array[0].split('').reverse().join('');
+        if (word != string) {
             return false;
-        }  else {
-            palindrome = true;
-        }  
-    }
-    return palindrome;
+        }
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            let word = array[i].split('').reverse().join('');
+            if (word != array[array.length - i]) {
+                return false;
+            }
+        }
+    } return true;
+}
+
+let string = "what about this";
+if (palindromes(string)) {
+    console.log("this is palindrome")
 };
-
-let word = "racecar";
-
-palindromes(word);
 
 // Do not edit below this line
 module.exports = palindromes;
