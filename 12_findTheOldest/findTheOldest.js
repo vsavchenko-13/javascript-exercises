5dcdc04de17db851bad2acd8072e8aaeaa97c242
vsavchenko-13 <v.savchenko1304@gmail.com>
2024-10-11 18:19:52 -0600

    Added solutions for the rest of the problems.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
+const findTheOldest = function(person) {
+    let oldest = 0;
+    let oldestName = '';
+    for (let i = 0; i < person.length; i++){
+        let age = person[i].yearOfDeath - person[i].yearOfBirth;
+        if (age > oldest){
+            oldest = age;
+            oldestName = person[i].name;
+        }
+    }
+    console.log(oldestName);
+    return oldestName;
 
 };
 
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+  findTheOldest(people);
+console.log(people.length);
 // Do not edit below this line
-module.exports = findTheOldest;
+//module.exports = findTheOldest;
